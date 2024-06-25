89c56f10f5ee498e321f4ac1320a25db9dc881ef
Yonjar <juan132y@gmail.com>
2024-06-25 10:11:27 -0600

    - Se ha agregado nueva información en la interfaz de partidos por liga

--- app/src/main/java/com/yonjar/futbolapp/leagues/ui/leagueDetail/navLeagueScreen/matchesInfoScreen/MatchesInfoScreen.kt
+++ app/src/main/java/com/yonjar/futbolapp/leagues/ui/leagueDetail/navLeagueScreen/matchesInfoScreen/MatchesInfoScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -21,7 +22,6 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
@@ -31,12 +31,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.yonjar.futbolapp.leagues.domain.models.MatchModel
 import com.yonjar.futbolapp.leagues.ui.leagueDetail.LoadingFun
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -51,9 +51,9 @@ fun MatchesInfoScreen(
 
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(
-        pageCount = {MatchesTabs.entries.size}
+        pageCount = { MatchesTabs.entries.size }
     )
-    val selectedTabIndex = remember{ derivedStateOf { pagerState.currentPage }}
+    val selectedTabIndex = remember { derivedStateOf { pagerState.currentPage } }
 
     LaunchedEffect(pagerState.currentPage) {
         val matchType = MatchesTabs.entries[pagerState.currentPage]
@@ -73,7 +73,7 @@ fun MatchesInfoScreen(
                     selectedTabIndex = selectedTabIndex.value,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(Color.Red)
+
                 ) {
                     MatchesTabs.entries.forEachIndexed { index, matchesTabs ->
                         Tab(selected = selectedTabIndex.value == index, onClick = {
@@ -81,16 +81,17 @@ fun MatchesInfoScreen(
                                 pagerState.animateScrollToPage(matchesTabs.ordinal)
                             }
                         },
-                            text = { Text(text = matchesTabs.text)
+                            text = {
+                                Text(text = matchesTabs.text)
                             }
                         )
                     }
                 }
 
-                when(val currentState = state.value){
-                    is MatchesState.Error -> ErrorFun(currentState,context)
+                when (val currentState = state.value) {
+                    is MatchesState.Error -> ErrorFun(currentState, context)
                     MatchesState.Loading -> LoadingFun()
-                    is MatchesState.Success -> SuccessFun(currentState,pagerState)
+                    is MatchesState.Success -> SuccessFun(currentState, pagerState)
                 }
             }
         }
@@ -104,21 +105,21 @@ fun SuccessFun(
     currentState: MatchesState.Success,
     pagerState: PagerState
 ) {
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Red)) {
-            LazyColumn {
-                if(currentState.matchesState?.isNotEmpty() == true){
-                    items(currentState.matchesState){matchInfo ->
-                        MatchItem(matchInfo)
-                    }
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        LazyColumn {
+            if (currentState.matchesState?.isNotEmpty() == true) {
+                items(currentState.matchesState) { matchInfo ->
+                    MatchItem(matchInfo)
                 }
             }
         }
-
     }
+
+}
 
 @Composable
 fun ErrorFun(currentState: MatchesState.Error, context: Context) {
@@ -127,12 +128,52 @@ fun ErrorFun(currentState: MatchesState.Error, context: Context) {
 
 @Composable
 fun MatchItem(matchInfo: MatchModel) {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-        AsyncImage(model = matchInfo.teamHome?.teamImage, contentDescription =  matchInfo.teamHome?.name)
-        Text(text = "VS", fontSize = 48.sp, fontWeight = FontWeight.ExtraBold)
-        AsyncImage(model = matchInfo.teamAway?.teamImage, contentDescription =  matchInfo.teamAway?.name)
+    Column(modifier = Modifier.padding(vertical = 20.dp)) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(text = matchInfo.name, fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = matchInfo.teamHome?.teamImage,
+                contentDescription = matchInfo.teamHome?.name,
+                modifier = Modifier.size(100.dp)
+            )
+            Text(text = "VS", fontSize = 36.sp, fontWeight = FontWeight.ExtraBold)
+            AsyncImage(
+                model = matchInfo.teamAway?.teamImage,
+                contentDescription = matchInfo.teamAway?.name,
+                modifier = Modifier.size(100.dp)
+            )
+        }
+        if(matchInfo.result?.isNotBlank() == true){
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = matchInfo.result,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }else{
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = matchInfo.date,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+
     }
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-        Text(text = matchInfo.name, fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-    }
+
+
 }
